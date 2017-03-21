0cc35d01b54fff1fb321391fbc20f87fe85e3eec
Liang-Bo Wang <me@liang2.tw>
2017-03-21 12:22:47 -0500

    Use meter-based mapping

--- update_taxi_trip_with_geom.sql
+++ update_taxi_trip_with_geom.sql
@@ -1,9 +1,9 @@
-SELECT AddGeometryColumn('taxi_trips', 'pickup_loc', 4326, 'POINT', 2);
-SELECT AddGeometryColumn('taxi_trips', 'dropoff_loc', 4326, 'POINT', 2);
+SELECT AddGeometryColumn('taxi_trips', 'pickup_loc', 3627, 'POINT', 2);
+SELECT AddGeometryColumn('taxi_trips', 'dropoff_loc', 3627, 'POINT', 2);
 
 UPDATE taxi_trips
-SET pickup_loc = ST_SetSRID(ST_MakePoint(pickup_lon, pickup_lat), 4326),
-    dropoff_loc = ST_SetSRID(ST_MakePoint(dropoff_lon, dropoff_lat), 4326);
+SET pickup_loc = ST_Transform(ST_SetSRID(ST_MakePoint(pickup_lon, pickup_lat), 4326), 3627),
+    dropoff_loc = ST_Transform(ST_SetSRID(ST_MakePoint(dropoff_lon, dropoff_lat), 4326), 3627);
 
 CREATE INDEX taxi_trips_pickup_gist
   ON taxi_trips
