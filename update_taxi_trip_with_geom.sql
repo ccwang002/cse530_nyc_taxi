3a8862fde4007a4d1097ea5f24b1e1f71ff664b2
Liang-Bo Wang <me@liang2.tw>
2017-03-23 20:25:08 -0500

    Exclude errorenous GPS locations

--- update_taxi_trip_with_geom.sql
+++ update_taxi_trip_with_geom.sql
@@ -3,7 +3,11 @@ SELECT AddGeometryColumn('taxi_trips', 'dropoff_loc', 3627, 'POINT', 2);
 
 UPDATE taxi_trips
 SET pickup_loc = ST_Transform(ST_SetSRID(ST_MakePoint(pickup_lon, pickup_lat), 4326), 3627),
-    dropoff_loc = ST_Transform(ST_SetSRID(ST_MakePoint(dropoff_lon, dropoff_lat), 4326), 3627);
+    dropoff_loc = ST_Transform(ST_SetSRID(ST_MakePoint(dropoff_lon, dropoff_lat), 4326), 3627)
+WHERE abs(pickup_lon - -73.97) < 2
+  AND abs(dropoff_lon - -73.97) < 2
+  AND abs(pickup_lat - 40.75) < 2
+  AND abs(dropoff_lat - 40.75) < 2;
 
 CREATE INDEX taxi_trips_pickup_gist
   ON taxi_trips
